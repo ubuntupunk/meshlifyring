0c4671e7854716877a14272fe4b7b426b85da8e7
ubuntupunk <ubuntpunk@gmail.com>
2023-09-20 09:43:16 +0200

    js

--- _lambda/next.js
+++ _lambda/next.js
@@ -1,8 +1,21 @@
-import { redirect, getNext, getRandom } from './utils'
+import { redirect, getNext, getRandom } from './utils';
 
 exports.handler = function(event, context, callback) {
     const { referer } = event.headers
-    const site = getNext(referer) || getRandom()
+    const site = getNext(referer) || getRandom();
+    
 
-    callback(null, redirect(site))
+if (!site) {
+    // Handle the case where no valid site URL is available
+    callback(null, {
+        statusCode: 500, // Internal Server Error
+        body: JSON.stringify({ error: 'Unable to determine redirect URL' }),
+    });
+    return;
 }
+
+// Ensure the site URL uses HTTPS (if possible)
+const secureSite = site.startsWith('https://') ? site : `https://${site}`;
+
+callback(null, redirect(secureSite));
+};
--- _lambda/prev.js
+++ _lambda/prev.js
@@ -1,8 +1,21 @@
-import { redirect, getPrevious, getRandom } from './utils'
+import { redirect, getPrevious, getRandom } from './utils';
 
 exports.handler = function(event, context, callback) {
     const { referer } = event.headers
-    const site = getPrevious(referer) || getRandom()
+    const site = getPrevious(referer) || getRandom();
+
+    if (!site) {
+        // Handle the case where no valid site URL is available
+        callback(null, {
+            statusCode: 500, // Internal Server Error
+            body: JSON.stringify({ error: 'Unable to determine redirect URL' }),
+        });
+        return;
+    }
+    
+    // Ensure the site URL uses HTTPS (if possible)
+    const secureSite = site.startsWith('https://') ? site : `https://${site}`;
+
 
     callback(null, redirect(site))
 }
